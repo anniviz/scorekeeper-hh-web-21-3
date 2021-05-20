b5fbce751d01fa05bb6ee32619de36eb6127387c
Annemarie Wisbar <a.wisbar@gmail.com>
2021-05-20 13:32:43 +0200

    Start working on function endGame

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import Form from './components/Form'
 import Navigation from './components/Navigation'
 import styled from 'styled-components'
+import Header from './components/Header'
+import Player from './components/Player'
+import Button from './components/Button'
 
 function App() {
   const [players, setPlayers] = useState([])
@@ -11,6 +14,8 @@ function App() {
     { title: 'History', id: 'history' },
   ]
   const [currentPageId, setCurrentPageId] = useState(pages[0].id)
+  const [currentGame, setCurrentGame] = useState('')
+  const [savedGames, setSavedGames] = useState([''])
 
   return (
     <>
@@ -23,6 +28,25 @@ function App() {
             currentPageId={currentPageId}
           ></Navigation>
         </PagePlay>
+      )}
+      {currentPageId === 'game' && (
+        <>
+          <Header>{currentGame}</Header>
+          <PlayerList>
+            {players.map((player, index) => (
+              <li key={player.name}>
+                <Player
+                  onMinus={() => updateScore(index, -1)}
+                  onPlus={() => updateScore(index, 1)}
+                  name={player.name}
+                  score={player.score}
+                />
+              </li>
+            ))}
+          </PlayerList>
+          <Button onClick={resetScores}>Reset scores</Button>
+          <Button onClick={endGame}>End game</Button>
+        </>
       )}
     </>
   )
@@ -52,9 +76,16 @@ function App() {
     const players = gameObject.player.split(',').map(name => name.trim())
     const playerWithScore = players.map(player => ({ name: player, score: 0 }))
     setPlayers(playerWithScore)
-    // console.log(playerWithScore)
+    setCurrentGame(gameObject.game)
+    setCurrentPageId('game')
   }
+
   function handleNavigation() {}
+
+  function endGame() {
+    setSavedGames([...savedGames, {}])
+    setCurrentPageId('history')
+  }
 }
 
 const PagePlay = styled.div`
@@ -62,6 +93,13 @@ const PagePlay = styled.div`
   grid-template-rows: auto min-content;
   height: 100vh;
   gap: 20px;
+`
+const PlayerList = styled.ul`
+  display: grid;
+  align-content: start;
+  gap: 10px;
+  list-style: none;
+  padding: 0;
 `
 
 export default App
